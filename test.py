25ce5d2831221f24f8e9aea01d5d630dbd3638a5
Nicholas Petty <nick@ihackeverything.com>
2015-08-21 16:11:38 -0500

    Initial Commit

---
+++ test.py
@@ -0,0 +1,21 @@
+from timeclock import Clock
+import time
+clock = Clock('EST')
+
+# print clock.now()
+#
+# print clock.now(tz='GMT', format='%H')
+#
+# print clock.now()
+format = "%Y %m %d %H %M %S"
+
+# print "Time until the 21th: "
+# print clock.elTime(clock.now(format), "2015 08 21 16 01 01", format=format)
+
+# print "Time since the 20th: "
+# print clock.elTime("2016 08 15 01 01 01", clock.now(format), format=format)
+then = clock.now(format)
+time.sleep(10)
+now = clock.now(format)
+
+print clock.elTime(then, now, format=format)
---
+++ timeclock/timeclock.py
@@ -0,0 +1,28 @@
+################################
+# Date/Time Wrapper for Python #
+################################
+#      Format Directives:      #
+# https://docs.python.org/2/library/datetime.html#strftime-and-strptime-behavior
+################################
+
+import time
+from datetime import datetime, timedelta
+import os
+
+class Clock:
+
+    def __init__(self, cZone):
+        self.cZone = cZone
+
+    def now(self, format="%H:%M:%S - %m/%d/%Y", tz=None):
+        if tz == None:
+            os.environ['TZ'] = self.cZone
+        else:
+            os.environ['TZ'] = tz
+
+        return time.strftime(str(format))
+
+    def elTime(self, date1, date2, format="%Y %m %d %H %M %S"):
+        td = datetime.strptime(date2, format) - datetime.strptime(date1, format)
+        #return dict object
+        return {"Hour":td.seconds//3600, "Min":(td.seconds//60)%60 , "Sec":td.seconds}
